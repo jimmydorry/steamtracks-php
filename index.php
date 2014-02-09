f4038c1f6fe2290482e625aa50ec1eb263dba561
jimmydorry <jimmydorry@hotmail.com>
2014-02-09 23:40:13 +1000

    Fixed request payloads

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 require_once('./functions.php');
-
-$token_getfields = array('return_steamid32=true');
-$token_response = steamtracks_curl('signup/token', 'GET', $token_getfields);
-
-if(!empty($token_response['result']['token'])){
-	$token = $token_response['result']['token'];
-	echo '<a href="https://steamtracks.com/appauth/'.$token.'">Grant access to Dota MMR</a>';
+try{
+	$token_getfields = array(
+		'return_steamid32' => 'true');
+	$token_response = steamtracks_curl('signup/token', 'GET', $token_getfields);
+	
+	if(!empty($token_response['result']['token'])){
+		$token = $token_response['result']['token'];
+		echo '<a href="https://steamtracks.com/appauth/'.$token.'">Grant access to Dota MMR</a>';
+	}
+	else{
+		var_dump($token_response);
+	}
 }
-else{
-	var_dump($token_response);
+catch (Exception $e){
+	echo $e->getMessage();
 }
 ?>
