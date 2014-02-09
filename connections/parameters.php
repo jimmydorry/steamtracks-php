2399c2568b54d90e1128b2406a2508ea93244daa
jimmydorry <jimmydorry@hotmail.com>
2014-02-09 22:21:05 +1000

    Added in mysql

--- connections/parameters.php
+++ connections/parameters.php
@@ -1,4 +1,9 @@
 <?php
 $steamtracks_api_key = 'API_KEY';
 $steamtracks_api_secret = 'API_SECRET';
+
+$hostname = "localhost";
+$database = "dota2_sigs";
+$username = "DB_USERNAME";
+$password = "DB_PASSWORD";
 ?>
